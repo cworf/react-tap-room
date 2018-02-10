34692b1cd6417773a95a5201caac3d5a3a8c653b
cworf <colin.worf@gmail.com>
2018-02-09 19:22:49 -0800

    add materialize framework

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const webpack = require('webpack');
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 module.exports = {
 
@@ -9,6 +10,7 @@ module.exports = {
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
+    'typeface-roboto',
     resolve(__dirname, 'src', 'index.jsx')
   ],
 
@@ -47,6 +49,35 @@ module.exports = {
           ]
         }
       },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,   //to support eg. background-image property
+        loader:"file-loader",
+        query:{
+          name:'[name].[ext]',
+          outputPath:'images/'
+          //the images will be emmited to public/assets/images/ folder
+          //the images will be put in the DOM <style> tag as eg. background: url(assets/images/image.png);
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    //to support @font-face rule
+        loader: "url-loader",
+        query:{
+          limit:'10000',
+          name:'[name].[ext]',
+          outputPath:'fonts/'
+          //the fonts will be emmited to public/assets/fonts/ folder
+          //the fonts will be put in the DOM <style> tag as eg. @font-face{ src:url(assets/fonts/font.ttf); }
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader","css-loader"]
+      }
     ],
   },
 
@@ -61,4 +92,4 @@ module.exports = {
     }),
   ]
 };
-/*eslint-enable */  
+/*eslint-enable */
