e792219b63ed2bbe31952afd27bcdff7126ffb11
cworf <colin.worf@gmail.com>
2018-02-11 01:13:01 -0800

    add keg fields

--- src/components/pages/AddKeg.jsx
+++ src/components/pages/AddKeg.jsx
@@ -6,13 +6,25 @@ function AddKeg() {
 
   return (
     <div >
-      <Typography variant="display1" gutterBottom>
+      <Typography style={{marginTop:'30px'}} variant="display1" gutterBottom>
         Check out our Beer!
       </Typography>
-      <form>
-        <Paper style={{textAlign:'center'}}>
+      <form style={{width: '30%', margin: '50px auto'}}>
+        <Paper style={{textAlign:'center', padding:'30px'}}>
           <div>
             <input placeholder="name" />
+          </div>
+          <div>
+            <input placeholder="brand" />
+          </div>
+          <div>
+            <input placeholder="price" />
+          </div>
+          <div>
+            <input placeholder="abv" />
+          </div>
+          <div>
+            <input placeholder="pints" />
           </div>
         </Paper>
       </form>
