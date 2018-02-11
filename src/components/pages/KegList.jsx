6c9f529674c922f63ad9694c5f040c1cdb3a7f44
cworf <colin.worf@gmail.com>
2018-02-10 19:14:14 -0800

    connect map to Keg component

--- src/components/pages/KegList.jsx
+++ src/components/pages/KegList.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import Keg from '../ui/Keg';
 
 const masterList = [
-  {name: 'IPA', brand: 'ninkasi', price: 6, apv: 10, pints: 12},
-  {name: 'Lager', brand: 'rolling rock', price: 3, apv: 8, pints: 12},
-  {name: 'Porter', brand: 'porters r us', price: 5, apv: 7, pints: 12},
-  {name: 'Stout', brand: 'guiness', price: 5, apv: 6, pints: 12},
-  {name: 'Pale Ale', brand: 'pyramid', price: 4, apv: 5, pints: 12}
+  {name: 'Delicious IPA', brand: 'ninkasi', price: 6, apv: 10, pints: 12},
+  {name: 'Amazing Lager', brand: 'rolling rock', price: 3, apv: 8, pints: 12},
+  {name: 'Scrumptous Porter', brand: 'porters r us', price: 5, apv: 7, pints: 12},
+  {name: 'Believable Stout', brand: 'guiness', price: 5, apv: 6, pints: 12},
+  {name: 'Fake Pale Ale', brand: 'pyramid', price: 4, apv: 5, pints: 12}
 ];
 
 function KegList() {
   return (
     <div>
       {masterList.map( (keg, index) =>
-        <div key={index}>{keg.name}</div>
+        <Keg key={index}
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          apv={keg.apv}
+          pints={keg.pints}/>
       )}
     </div>
   );
