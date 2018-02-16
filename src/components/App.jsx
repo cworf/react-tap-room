12e73b07f5801859b7021598b94a2d58d39d58b0
cworf <colin.worf@gmail.com>
2018-02-16 10:11:40 -0800

    refactor: move state to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,27 +24,43 @@ const theme = createMuiTheme({
   },
 });
 
-function App() {
-  return (
-    <div>
-      <style jsx global>{`
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-      `}</style>
-      <Reboot />
-      <MuiThemeProvider theme={theme}>
-        <div>
-          <MenuBar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/admin" component={Admin} />
-          </Switch>
-        </div>
-      </MuiThemeProvider>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      masterList : [
+        {name: 'Delicious IPA', brand: 'ninkasi', price: 6, abv: 10, pints: 12},
+        {name: 'Amazing Lager', brand: 'rolling rock', price: 3, abv: 8, pints: 12},
+        {name: 'Scrumptous Porter', brand: 'porters r us', price: 5, abv: 7, pints: 12},
+        {name: 'Believable Stout', brand: 'guiness', price: 5, abv: 6, pints: 12},
+        {name: 'Fake Pale Ale', brand: 'pyramid', price: 4, abv: 5, pints: 12}
+      ]
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <style jsx global>{`
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+        `}</style>
+        <Reboot />
+        <MuiThemeProvider theme={theme}>
+          <div>
+            <MenuBar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/admin" component={Admin} />
+            </Switch>
+          </div>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ui/KegList.jsx
+++ src/components/ui/KegList.jsx
@@ -5,13 +5,6 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 
-const masterList = [
-  {name: 'Delicious IPA', brand: 'ninkasi', price: 6, abv: 10, pints: 12},
-  {name: 'Amazing Lager', brand: 'rolling rock', price: 3, abv: 8, pints: 12},
-  {name: 'Scrumptous Porter', brand: 'porters r us', price: 5, abv: 7, pints: 12},
-  {name: 'Believable Stout', brand: 'guiness', price: 5, abv: 6, pints: 12},
-  {name: 'Fake Pale Ale', brand: 'pyramid', price: 4, abv: 5, pints: 12}
-];
 
 const styles = theme => ({
   root: {
